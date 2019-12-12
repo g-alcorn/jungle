2dfe1bbbcb95a252569f286f99f8c0c17441260a
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-11 19:50:27 -0500

    passes two AR query results to view file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
 
   def show
+    @data ||= {
+      product_count: Product.sum(:quantity),
+      category_count: Category.distinct.count}
   end
 end
