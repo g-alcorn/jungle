f0ca6816c243dd17e66dca92f8473c5715c76e23
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-12 18:56:22 -0500

    formatting

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,12 @@ class Admin::DashboardController < ApplicationController
   def show
     @data ||= {
       product_count: Product.sum(:quantity),
-      category_count: Category.distinct.count}
+      category_count: Category.distinct.count
+    }
+    @categories ||= Category.all
+  end
+
+  def index
+    @categories = Category.order(id: :desc).all
   end
 end
