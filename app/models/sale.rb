483d81b03e35e8b29ac683646374f404eafdb5a5
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-14 16:16:01 -0500

    class method to determine if active

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,19 @@
 class Sale < ActiveRecord::Base
+
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.today, Date.today)
+  end
+
+  def finished?
+    ends_on < Date.today
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
+  def upcoming?
+    starts_on > Date.today
+  end
+
 end
