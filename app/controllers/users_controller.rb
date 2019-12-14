258fa77be577ce156d8512a69e1010b49f7c9cf4
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-12-14 12:57:28 -0500

    controllers properly manage login status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to 'products'
+      redirect_to '/products'
     else
-      redirect_to 'register'
+      redirect_to '/register'
     end
   end
 
